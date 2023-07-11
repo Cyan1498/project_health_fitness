7125995bd0c6d62e21255ed13f4a13f424b15595
Cyan <yancerna1498@gmail.com>
2023-07-11 08:58:35 -0500

    Initial commit

---
+++ app/src/main/java/com/grupoc/project_health_fitness/ui/activities/InicioActivity.kt
@@ -0,0 +1,41 @@
+package com.grupoc.project_health_fitness.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.grupoc.project_health_fitness.R
+import com.grupoc.project_health_fitness.databinding.ActivityInicioBinding
+
+class InicioActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInicioBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInicioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val drawerLayout = binding.drawerLayout
+        val navigationView = binding.navView
+        val navController = findNavController(R.id.nav_fragment)
+
+        navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+
+    }
+}
