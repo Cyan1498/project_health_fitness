d0c9c8fe385f1e8aab629fac7bba73e897795768
ronnydp <ronnydp742@gmail.com>
2023-07-13 19:33:31 -0500

    Crear Usuario

--- app/src/main/java/com/grupoc/project_health_fitness/ui/fragments/CrearUsuarioFragment.kt
+++ app/src/main/java/com/grupoc/project_health_fitness/ui/fragments/CrearUsuarioFragment.kt
@@ -1,60 +1,55 @@
 package com.grupoc.project_health_fitness.ui.fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.grupoc.project_health_fitness.R
+import com.grupoc.project_health_fitness.databinding.FragmentCrearUsuarioBinding
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CrearUsuarioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CrearUsuarioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentCrearUsuarioBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onResume() {
+        super.onResume()
+        val genero = resources.getStringArray(R.array.genero)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, genero)
+        binding.autoCompleteGenero.setAdapter(arrayAdapter)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_usuario, container, false)
+        _binding = FragmentCrearUsuarioBinding.inflate(inflater, container, false)
+        binding.etdate.setOnClickListener{
+            showDatePicker()
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CrearUsuarioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CrearUsuarioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireActivity(),
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val formattedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                binding.etdate.setText(formattedDate)
+            },
+            year,
+            month,
+            day
+        )
+        datePickerDialog.show()
     }
+
 }
