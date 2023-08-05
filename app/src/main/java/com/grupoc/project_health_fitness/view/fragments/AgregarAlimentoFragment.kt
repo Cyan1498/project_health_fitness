75ef8baf23ab5d404fc51bac975631f4d410b9df
SebastianFM1 <45723245+SebastianFM1@users.noreply.github.com>
2023-08-04 21:01:41 -0500

    Avance

--- app/src/main/java/com/grupoc/project_health_fitness/view/fragments/AgregarAlimentoFragment.kt
+++ app/src/main/java/com/grupoc/project_health_fitness/view/fragments/AgregarAlimentoFragment.kt
@@ -1,34 +1,15 @@
 package com.grupoc.project_health_fitness.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
 import com.grupoc.project_health_fitness.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AgregarAlimentoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AgregarAlimentoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class AgregarAlimentoFragment : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +19,16 @@ class AgregarAlimentoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_agregar_alimento, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AgregarAlimentoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AgregarAlimentoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn_Añadir = view.findViewById<Button>(R.id.registrarAlimento_button)
+        val edit_ingAlim=view.findViewById<EditText>(R.id.ingresarAlimento_txt)
+        val edit_cal=view.findViewById<EditText>(R.id.ingresarCalorias_txt)
+        val edit_gras=view.findViewById<EditText>(R.id.ingresarGrasa_txt)
+        val edit_carb=view.findViewById<EditText>(R.id.ingresarCarbohidratos_txt)
+        val edit_prot=view.findViewById<EditText>(R.id.ingresarProteina_txt)
+
+
     }
 }
