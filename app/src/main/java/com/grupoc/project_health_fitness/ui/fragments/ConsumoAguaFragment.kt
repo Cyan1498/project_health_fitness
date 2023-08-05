8587bc5ce07bbfcbcda05c772d990c9dc2d80f1e
SebastianFM1 <45723245+SebastianFM1@users.noreply.github.com>
2023-08-04 21:01:41 -0500

    Avance

--- app/src/main/java/com/grupoc/project_health_fitness/ui/fragments/ConsumoAguaFragment.kt
+++ app/src/main/java/com/grupoc/project_health_fitness/ui/fragments/ConsumoAguaFragment.kt
@@ -4,54 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.grupoc.project_health_fitness.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConsumoAguaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-class ConsumoAguaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class ConsumoAguaFragment : DialogFragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_consumo_agua, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConsumoAguaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConsumoAguaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn_añadir=view.findViewById<Button>(R.id.registrarAgua_button)
+        val edit_agua=view.findViewById<EditText>(R.id.ingresarAgua_txt)
     }
 }
