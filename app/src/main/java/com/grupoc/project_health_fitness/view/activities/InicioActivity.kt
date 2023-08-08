16d83d8ff1c2dd4eed7311a91d8712b5f2ab9a85
ronnydp <ronnydp742@gmail.com>
2023-08-07 23:04:50 -0500

    change navigation

--- app/src/main/java/com/grupoc/project_health_fitness/view/activities/InicioActivity.kt
+++ app/src/main/java/com/grupoc/project_health_fitness/view/activities/InicioActivity.kt
@@ -4,22 +4,32 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.grupoc.project_health_fitness.MainActivity
 import com.grupoc.project_health_fitness.R
 import com.grupoc.project_health_fitness.databinding.ActivityInicioBinding
+import com.grupoc.project_health_fitness.view.fragments.ConsumoFragment
+import com.grupoc.project_health_fitness.view.fragments.RecordatorioFragment
+import com.grupoc.project_health_fitness.view.fragments.ResumenFragment
 
-class InicioActivity : AppCompatActivity() {
+class InicioActivity : AppCompatActivity(){
 
+    private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityInicioBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var toggle: ActionBarDrawerToggle
@@ -31,6 +41,7 @@ class InicioActivity : AppCompatActivity() {
         binding = ActivityInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+//        binding.navView.setNavigationItemSelectedListener(this)
         firebaseAuth = Firebase.auth
         FirebaseUser = FirebaseAuth.getInstance()
 
@@ -58,16 +69,45 @@ class InicioActivity : AppCompatActivity() {
 
         showUserInfo(email.toString(), user.toString())
 
-        navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_salir -> {
-                    showLogoutConfirmationDialog()
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.consumoFragment -> {
+                    openFragment(ConsumoFragment())
                     true
                 }
-
-                else -> false
+                R.id.recordatorioFragment -> {
+                    openFragment(RecordatorioFragment())
+                    true
+                }
+                R.id.nav_salir -> {
+                    showLogoutConfirmationDialog()
+                    true // Devuelve true para indicar que el evento se ha manejado correctamente
+                }
+                else -> false // Devuelve false si no se maneja el evento
             }
         }
+    }
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.consumoFragment -> openFragment(ConsumoFragment())
+////                R.id.fitnessFragment -> openFragment(FitnessFragment())
+////                R.id.recorridoFragment -> openFragment(RecorridoFragment())
+//            R.id.recordatorioFragment -> openFragment(RecordatorioFragment())
+//            R.id.nav_salir -> {
+//                showLogoutConfirmationDialog()
+//            }
+//        }
+//        binding.drawerLayout.closeDrawers()
+//        return true
+//    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentManager = this.supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_fragment, fragment)
+        fragmentTransaction.commit()
+        binding.drawerLayout.closeDrawers()
     }
 
     private fun logoutMenu() {
